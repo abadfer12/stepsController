865b7b0d3a8485e51543f03781dc6448a9cd4c06
abadfer12 <abadfer12@gmail.com>
2014-05-28 18:26:28 +0200

    sql

---
+++ db/officeDatabase.sql
@@ -0,0 +1,18 @@
+drop database officeDatabase;
+
+CREATE DATABASE officeDatabase;
+
+GRANT ALL ON officeDatabase.* TO springappuser@'%' IDENTIFIED BY 'pspringappuser';
+GRANT ALL ON officeDatabase.* TO springappuser@localhost IDENTIFIED BY 'pspringappuser';
+
+USE officeDatabase;
+
+CREATE TABLE office (
+  id INTEGER PRIMARY KEY,
+  idBank INTEGER,
+  idOffice INTEGER,
+  address varchar(50),
+  totalExpenses INTEGER,
+  totalIncome INTEGER
+);
+CREATE INDEX office_address ON office(address);
